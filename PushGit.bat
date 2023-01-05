0ef5292b53449d4971409e8975ce78d04e50a467
iz2kk <hqvblog@gmail.com>
2023-01-05 12:27:29 +0700

    AKEBI09 05/01/2023 12:27:29,74

--- PushGit.bat
+++ PushGit.bat
@@ -1,9 +1,14 @@
 @echo off
 echo Nhap ten branch:
-set /p Branch=
+set /p Branch="Nhap branch tai day: "
+
+
+set mydate=%date%
+set mytime=%time%
+
 @echo on
 git add *
-git commit -m "%Branch%"
+git commit -m "%Branch% %mydate% %mytime%"
 git checkout -b "%Branch%"
 git push izi "%Branch%"
 pause.
