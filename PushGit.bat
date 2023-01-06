e7425b72bb2452b562c2ce75cf90c4f13d60bcd5
iz2kk <hqvblog@gmail.com>
2023-01-06 12:18:14 +0700

    AKEBI15-06/01/2023 12:18:13,97

--- PushGit.bat
+++ PushGit.bat
@@ -1,4 +1,5 @@
 @echo off
+:Repush
 echo Nhap ten branch:
 set /p Branch="Nhap branch tai day: "
 
@@ -11,4 +12,10 @@ git add *
 git commit -m "%Branch%-%mydate% %mytime%"
 git checkout -b "%Branch%"
 git push izi "%Branch%"
+
+
+set /p command="Nhap Lenh Tai Day"
+if command == cl(
+	goto Repush
+)
 pause.
